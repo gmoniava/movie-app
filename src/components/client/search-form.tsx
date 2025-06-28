c71a95d03db1319f42677fc15b866675da36626f
gmoniava <moniava.g@gmail.com>
2025-06-28 20:00:55 +0400

    layout changes

--- src/components/client/search-form.tsx
+++ src/components/client/search-form.tsx
@@ -112,84 +112,85 @@ export default function Search({}: any) {
       <div className="h-full p-5">
         <div className="text-xl font-semibold mb-5">Search movies</div>
 
-        <form action={formAction} className="flex flex-col gap-4 md:flex-row md:gap-8">
-          {/* First column */}
-          <div className="flex flex-col gap-4 w-full md:w-1/2">
-            <div>
-              <label className="block">Name:</label>
-              <input
-                type="text"
-                name="name"
-                className="w-full border rounded border-slate-300 p-1"
-                placeholder="e.g. Inception"
-                defaultValue={formState.name}
-              />
-            </div>
-
-            <div>
-              <label className="block">Actors:</label>
-              <input
-                type="text"
-                name="actor"
-                className="w-full border rounded border-slate-300 p-1"
-                placeholder="e.g. Leonardo DiCaprio"
-                defaultValue={formState.actor}
-              />
-            </div>
-
-            <div>
-              <label className="block">Release Year Range:</label>
-              <div className="flex gap-2">
+        <form action={formAction}>
+          <div className="flex flex-col gap-4 md:flex-row md:gap-8">
+            {/* First column */}
+            <div className="flex flex-col gap-4 w-full md:w-1/2">
+              <div>
+                <label className="block">Name:</label>
                 <input
-                  type="number"
-                  name="release_year_from"
-                  className="w-1/2 border rounded border-slate-300 p-1"
-                  placeholder="From"
-                  defaultValue={formState.release_year_from}
+                  type="text"
+                  name="name"
+                  className="w-full border rounded border-slate-300 p-1"
+                  placeholder="e.g. Inception"
+                  defaultValue={formState.name}
                 />
+              </div>
+
+              <div>
+                <label className="block">Actors:</label>
                 <input
-                  type="number"
-                  name="release_year_to"
-                  className="w-1/2 border rounded border-slate-300 p-1"
-                  placeholder="To"
-                  defaultValue={formState.release_year_to}
+                  type="text"
+                  name="actor"
+                  className="w-full border rounded border-slate-300 p-1"
+                  placeholder="e.g. Leonardo DiCaprio"
+                  defaultValue={formState.actor}
+                />
+              </div>
+
+              <div>
+                <label className="block">Release Year Range:</label>
+                <div className="flex gap-2">
+                  <input
+                    type="number"
+                    name="release_year_from"
+                    className="w-1/2 border rounded border-slate-300 p-1"
+                    placeholder="From"
+                    defaultValue={formState.release_year_from}
+                  />
+                  <input
+                    type="number"
+                    name="release_year_to"
+                    className="w-1/2 border rounded border-slate-300 p-1"
+                    placeholder="To"
+                    defaultValue={formState.release_year_to}
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* Second column */}
+            <div className="flex flex-col gap-4 w-full md:w-1/2">
+              <div>
+                <label className="block">Description:</label>
+                <input
+                  type="text"
+                  name="description"
+                  className="w-full border rounded border-slate-300 p-1"
+                  placeholder="e.g. This is a drama movie.."
+                  defaultValue={formState.description}
+                />
+              </div>
+
+              <div>
+                <label className="block">Genres:</label>
+                <Select
+                  isMulti
+                  options={genreOptions}
+                  value={selectedGenres}
+                  onChange={(selected) => setSelectedGenres(selected || [])}
+                  className="w-full"
+                  classNamePrefix="react-select"
+                  placeholder="Select genres"
+                  name="genres"
                 />
               </div>
             </div>
           </div>
-
-          {/* Second column */}
-          <div className="flex flex-col gap-4 w-full md:w-1/2">
-            <div>
-              <label className="block">Description:</label>
-              <input
-                type="text"
-                name="description"
-                className="w-full border rounded border-slate-300 p-1"
-                placeholder="e.g. This is a drama movie.."
-                defaultValue={formState.description}
-              />
-            </div>
-
-            <div>
-              <label className="block">Genres:</label>
-              <Select
-                isMulti
-                options={genreOptions}
-                value={selectedGenres}
-                onChange={(selected) => setSelectedGenres(selected || [])}
-                className="w-full"
-                classNamePrefix="react-select"
-                placeholder="Select genres"
-                name="genres"
-              />
-            </div>
-
-            <div className="text-right pt-10">
-              <Button primary nativeProps={{ type: "submit", disabled: isPending, style: { width: 150 } }}>
-                {isPending ? "Searching..." : "Search"}
-              </Button>
-            </div>
+          <div className="text-right pt-10">
+            <Button primary nativeProps={{ type: "submit", disabled: isPending, style: { width: 150 } }}>
+              {isPending ? "Searching..." : "Search"}
+            </Button>
           </div>
         </form>
       </div>
