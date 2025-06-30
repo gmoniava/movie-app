1c7a29c895897f1f0337ff8cfd1acd8e227098de
gmoniava <moniava.g@gmail.com>
2025-06-30 21:42:47 +0400

    Change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 import "./globals.css";
 import { Providers } from "../components/client/providers";
 import { ClientOnly } from "@/components/client/client-only";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({
+  subsets: ["latin"],
+});
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full" suppressHydrationWarning>
+    <html lang="en" className={`h-full ${roboto.className}`} suppressHydrationWarning>
       <body className="h-full default-bg default-text">
         {" "}
         <Providers>
