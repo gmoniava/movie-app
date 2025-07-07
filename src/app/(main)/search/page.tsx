63c1cf2613ef9d976981e67ec471c3389d7a60d3
gmoniava <moniava.g@gmail.com>
2025-07-07 21:14:09 +0400

    change param type

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -2,12 +2,7 @@ import SearchForm from "@/components/client/search-form";
 import { Suspense } from "react";
 import Loading from "@/components/client/loading";
 import PaginatedMoviesList from "@/components/server/paginated-movies-list";
-export default async function Page(props: {
-  searchParams?: Promise<{
-    query?: string;
-    page?: string;
-  }>;
-}) {
+export default async function Page(props: { searchParams?: Promise<Record<string, any>> }) {
   const searchParams = await props.searchParams;
   return (
     <div className="h-full overflow-auto">
