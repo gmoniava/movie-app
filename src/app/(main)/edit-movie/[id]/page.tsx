aa07e7f703346505831e11aed2db5a4e09228dbe
gmoniava <moniava.g@gmail.com>
2025-08-28 21:54:56 +0400

    add some comments

--- src/app/(main)/edit-movie/[id]/page.tsx
+++ src/app/(main)/edit-movie/[id]/page.tsx
@@ -4,12 +4,18 @@ import AddMovie from "@/app/(main)/add-movie/page";
 export default async function Page(props: any) {
   const params = await props.params;
   const id = params.id;
+
+  // On this route, we get the we want to edit
   const movie = await getMovieById(id);
+
+  // If we got error, just show the error message.
   if ("error" in movie) {
     return <div>{movie.error}</div>;
   }
+
   return (
     <div>
+      {/* Render AddMovie component but pass movie as prop to indicate we are in edit mode. */}
       <AddMovie movie={movie} />
     </div>
   );
