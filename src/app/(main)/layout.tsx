91197e7051ce53596d169e4b32bf0e5e66aec090
gmoniava <moniava.g@gmail.com>
2025-08-31 20:27:16 +0400

    add shouldAnimate

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -12,22 +12,30 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   const [showSidebar, setShowSideBar] = useLocalStorage("movie-app", false);
+  const [shouldAnimate, setShouldAnimate] = React.useState(false);
 
   return (
     <div lang="en" className="h-full flex">
       <Sidebar
         isOpen={showSidebar}
+        shouldAnimate={shouldAnimate}
         close={() => {
           setShowSideBar(false);
         }}
       />
 
       <div
-        className={clsx("flex-1 flex flex-col h-full transition-[margin-left] duration-300 ease-in-out", {
+        className={clsx("flex-1 flex flex-col h-full", {
           "sm:ml-64": showSidebar,
+          "transition-[margin-left] duration-300 ease-in-out": shouldAnimate,
         })}
       >
-        <Header toggleSideBar={() => setShowSideBar(!showSidebar)} />
+        <Header
+          toggleSideBar={() => {
+            setShowSideBar(!showSidebar);
+            setShouldAnimate(true);
+          }}
+        />
         <div className="flex-1 min-h-0">{children}</div>
       </div>
     </div>
