3e222592774deb536075f92f1e68a6e0f9c8f8ba
gmoniava <moniava.g@gmail.com>
2025-10-25 18:45:47 +0400

    minor changes in the sidebar

--- src/components/client/sidebar.tsx
+++ src/components/client/sidebar.tsx
@@ -8,47 +8,48 @@ import clsx from "clsx";
 import { useAuth } from "@/components/client/auth-provider";
 import Overlay from "@/components/client/overlay";
 
+const navItems = [
+  {
+    href: "/",
+    label: "Home",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+      </svg>
+    ),
+  },
+  {
+    href: "/add-movie",
+    label: "Add movie",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path
+          fillRule="evenodd"
+          d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    href: "/search",
+    label: "Search movies",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path
+          fillRule="evenodd"
+          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+];
+
 export default function Page(props: any) {
   const { isAuthenticated, checkAuth } = useAuth();
   const [isPending, startTransition] = useTransition();
   const pathname = usePathname();
-  const navItems = [
-    {
-      href: "/",
-      label: "Home",
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-        </svg>
-      ),
-    },
-    {
-      href: "/add-movie",
-      label: "Add movie",
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path
-            fillRule="evenodd"
-            d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      href: "/search",
-      label: "Search movies",
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path
-            fillRule="evenodd"
-            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-  ];
 
   return (
     <div
@@ -60,14 +61,12 @@ export default function Page(props: any) {
         }
       )}
     >
-      {/* Backdrop for mobile */}
-      {/* Visible only on mobile when sidebar is open */}
+      {/* Backdrop for mobile - visible only on mobile when sidebar is open */}
       {props.isOpen && (
         <Overlay positionClass="left-64 top-0 bottom-0 right-0" responsiveClass={"md:hidden"} useLoading={false} />
       )}
 
-      {/* Close button */}
-      {/* Visible only on mobile when sidebar is open */}
+      {/* Close button - visible only on mobile when sidebar is open */}
       <div className="md:hidden w-64 flex justify-end pr-5">
         <button
           className="p-2 rounded-full hover:bg-gray-200 cursor-pointer"
@@ -89,7 +88,7 @@ export default function Page(props: any) {
         </button>
       </div>
 
-      {/* The nav items */}
+      {/* The menu items */}
       <div className="w-full mt-4 space-y-1">
         {navItems.map((item) => (
           <Link
